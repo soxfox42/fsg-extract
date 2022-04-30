ced0a0b2d8d4b3e8be91b1148119840952c152c1
Finn Coffey <finn.coffey@gmail.com>
2022-05-01 01:51:44 +1000

    Revert "Implement Seek on FsgReader?"
    This reverts commit 394b62e17c96bcc2a367dbc00bb1453c8e42eff2.

--- src/reader.rs
+++ src/reader.rs
@@ -1,29 +1,21 @@
 use std::collections::VecDeque;
 use std::fs::File;
-use std::io::{self, Read, Seek};
+use std::io::{self, Read};
 
 /// FSG IMG files may be split into multiple parts; this struct allows any number of parts to be read as one stream.
 pub struct FsgReader {
     parts: VecDeque<File>,
-    current_len: u64,
 }
 
 impl FsgReader {
     pub fn new(first_file: File) -> Self {
-        let current_len = first_file.metadata().unwrap().len();
         Self {
             parts: vec![first_file].into(),
-            current_len,
         }
     }
 
     pub fn add(&mut self, file: File) {
         self.parts.push_back(file)
-    }
-
-    fn next_file(&mut self) {
-        self.parts.pop_front();
-        self.current_len = self.parts.front().unwrap().metadata().unwrap().len();
     }
 }
 
@@ -35,32 +27,13 @@ impl Read for FsgReader {
 
         while let Some(file) = self.parts.front_mut() {
             match file.read(buf) {
-                Ok(0) => self.next_file(),
+                Ok(0) => {
+                    self.parts.pop_front();
+                }
                 r => return r,
             }
         }
 
         Ok(0)
-    }
-}
-
-impl Seek for FsgReader {
-    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
-        if let io::SeekFrom::End(_) = pos {
-            while self.parts.len() > 1 {
-                self.next_file();
-            }
-
-            return self.parts.front_mut().unwrap().seek(pos);
-        }
-
-        loop {
-            let new_pos = self.parts.front_mut().unwrap().seek(pos)?;
-            if new_pos >= self.current_len {
-                self.next_file();
-            } else {
-                return Ok(new_pos);
-            }
-        }
     }
 }
